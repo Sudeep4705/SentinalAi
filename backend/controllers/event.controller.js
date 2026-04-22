9531f48be9cd5d0bbb39ed5aed20450435364b2d
Sudeep <sudeepsudee828@gmail.com>
2026-04-22 10:33:13 +0530

    events updated

--- backend/controllers/event.controller.js
+++ backend/controllers/event.controller.js
@@ -3,10 +3,14 @@ const Event = require("../model/event")
 
 
 function generateEvents() {
-  const types = ["badge_swipe", "door_open", "fence_alert","tresspassing"];
+  const types = ["BADGE_SWIPE", "DOOR_OPEN", "FENCE_ALERT", "TRESSPASSING"];
+  const lastNight = new Date();
+  lastNight.setDate(lastNight.getDate() - 1); 
+  lastNight.setHours(2, 0, 0, 0); 
+
   return Array.from({ length: 10 }, (_, i) => ({
     type: types[Math.floor(Math.random() * types.length)],
-    timestamp: new Date(Date.now() - i * 60000),
+    timestamp: new Date(lastNight.getTime() + i * 60000), 
     location: {
       lat: 12.97 + Math.random() * 0.01,
       lng: 77.59 + Math.random() * 0.01
